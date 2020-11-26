883d368d326b6d45484defc40b9b43142e20628d
jina <psalvarajah@lcs.on.ca>
2020-11-26 09:51:32 -0500

    Got the JSON to work and bring different dog pictures every time the button is clicked.

---
+++ JSONExemplar/RandomDog.swift
@@ -0,0 +1,13 @@
+//
+//  RandomDog.swift
+//  JSONExemplar
+//
+//  Created by Salvarajah, Prajina on 2020-11-26.
+//
+
+import Foundation
+
+struct RandomDog: Codable {
+    var message: String
+    var status: String
+}
