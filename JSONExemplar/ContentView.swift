1a9e23aa375e43e835c0292d8deddf592a03927d
jina <psalvarajah@lcs.on.ca>
2020-11-26 08:55:58 -0500

    Initial Commit

---
+++ JSONExemplar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONExemplar
+//
+//  Created by Salvarajah, Prajina on 2020-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSONExemplar/JSONExemplarApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONExemplarApp.swift
+//  JSONExemplar
+//
+//  Created by Salvarajah, Prajina on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct JSONExemplarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
