d3246f9609ef487ab36c78ab5fb89d582efea0ab
jina <psalvarajah@lcs.on.ca>
2020-11-26 20:47:49 -0500

    I did a lot in this commit. First of all I created the file RandomFox for it to read the fox JSON I was taking. Then I created the 2 main functions called "fetchFoxCuteness" and "fetchFoxImage" which essentially is the same as the dog version but for the fox JSON. Finally I connected my picker to the different functions by using a switch statement.

--- JSONExemplar/ContentView.swift
+++ JSONExemplar/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var dogImage = UIImage()
+    @State private var foxImage = UIImage()
     @State private var typeOfAnimal = 2
     
     let typeOfAnimals = ["doggo", "fox"]
+        
     
     var body: some View {
         NavigationView {
@@ -27,7 +29,7 @@ struct ContentView: View {
                 
                 Button(action: {
                     // get a new dog
-                    fetchMoreCuteness()
+                    chooseTypeOfAnimal()
                 }, label: {
                     Text("More pls ; - ;")
                 })
@@ -43,8 +45,10 @@ struct ContentView: View {
             .navigationTitle("Bow WOW!")
         }
     }
+
+    // < ** Get the dog pictures ** >
     
-    // Get a random pooch pic!
+        // Get a random pooch pic!
     func fetchMoreCuteness() {
         
         // 1. Prepare a URLRequest to send our encoded data as JSON
@@ -86,7 +90,7 @@ struct ContentView: View {
         
     }
     
-    // Get the actual image data
+        // Get the actual image data
     func fetchImage(from address: String) {
         
         // 1. Prepare a URL that points to the image to be loaded
@@ -122,6 +126,109 @@ struct ContentView: View {
             
         }.resume()
         
+    }
+    
+    
+    // < ** Get the fox pictures ** >
+    
+        //Get a random fox pic
+    
+    func fetchFoxCuteness() {
+        
+        // 1. Prepare a URLRequest to send our encoded data as JSON
+        let url = URL(string: "https://randomfox.ca/floof/")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+        
+        // 2. Run the request and process the response
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            // handle the result here – attempt to unwrap optional data provided by task
+            guard let foxData = data else {
+                
+                // Show the error message
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+                
+                return
+            }
+            
+            // It seems to have worked? Let's see what we have
+            print(String(data: foxData, encoding: .utf8)!)
+            
+            // Now decode from JSON into an array of Swift native data types
+            if let decodedFoxData = try? JSONDecoder().decode(RandomFox.self, from: foxData) {
+                
+                print("Fox data decoded from JSON successfully")
+                print("URL is: \(decodedFoxData.image)")
+                
+                // Now fetch the image at the address we were given
+                fetchImage(from: decodedFoxData.image)
+                
+            } else {
+                
+                print("Invalid response from server.")
+            }
+            
+        }.resume()
+        
+    }
+    
+        // Get the actual image data
+    func fetchFoxImage(from address: String) {
+        
+        // 1. Prepare a URL that points to the image to be loaded
+        let url = URL(string: address)!
+        
+        // 2. Run the request and process the response
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            // handle the result here – attempt to unwrap optional data provided by task
+            guard let imageData = data else {
+                
+                // Show the error message
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+                
+                return
+            }
+            
+            // Update the UI on the main thread
+            DispatchQueue.main.async {
+                
+                // Attempt to create an instance of UIImage using the data from the server
+                guard let loadedFox = UIImage(data: imageData) else {
+                    
+                    // If we could not load the image from the server, show a default image
+                    foxImage = UIImage(named:"fox")!
+                    return
+                }
+                
+                // Set the image loaded from the server so that it shows in the user interface
+                foxImage = loadedFox
+                
+            }
+            
+        }.resume()
+        
+    }
+    
+// a switch statement that should hook up with the picker to get a animal chosen
+   
+    func chooseTypeOfAnimal() {
+
+    switch typeOfAnimal {
+    case 0:
+    // show a dog picture
+    return
+        fetchMoreCuteness()
+    case 1:
+    // show fox pictures
+    return
+        fetchFoxCuteness()
+
+    default:
+        break
+        }
     }
 }
 
---
+++ JSONExemplar/RandomFox.swift
@@ -0,0 +1,15 @@
+//
+//  RandomFox.swift
+//  JSONExemplar
+//
+//  Created by Salvarajah, Prajina on 2020-11-26.
+//
+
+import Foundation
+
+struct RandomFox: Codable {
+    var image: String
+    var link: String
+    
+}
+
